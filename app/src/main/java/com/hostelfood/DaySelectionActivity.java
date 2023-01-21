b0e253d5b9553057b2de281142b2fa2652d05d7a
Shivam Sharma <shivamsharma707@yahoo.com>
2023-01-22 00:38:43 +0530

    day selection screen complete

---
+++ app/src/main/java/com/hostelfood/DaySelectionActivity.java
@@ -0,0 +1,14 @@
+package com.hostelfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaySelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_selection);
+    }
+}
--- app/src/main/java/com/hostelfood/SplashActivity.java
+++ app/src/main/java/com/hostelfood/SplashActivity.java
@@ -1,7 +1,5 @@
 package com.hostelfood;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,6 +9,8 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -19,22 +19,16 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         Window window = getWindow();
-        window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         TextView tvTitle = findViewById(R.id.tv_title);
         Animation slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide);
         tvTitle.startAnimation(slideAnimation);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Intent daySelectionIntent = new Intent(this, )
-                //startActivity(intent)
-                //finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent daySelectionIntent = new Intent(SplashActivity.this, DaySelectionActivity.class);
+            startActivity(daySelectionIntent);
+            finish();
         }, 4000);
     }
 }
